fb81a9e7ef41638c28165a619ff6322a4bd26250
Oleksandr Kurtsev <kurtsev0103@gmail.com>
2021-09-12 13:23:56 +0200

    Added GodotFacebookAuth static library

---
+++ GodotFacebookAuth/GodotFacebookAuth.h
@@ -0,0 +1,12 @@
+//
+//  GodotFacebookAuth.h
+//  GodotFacebookAuth
+//
+//  Created by Oleksandr Kurtsev on 12/09/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GodotFacebookAuth : NSObject
+
+@end
---
+++ GodotFacebookAuth/GodotFacebookAuth.m
@@ -0,0 +1,12 @@
+//
+//  GodotFacebookAuth.m
+//  GodotFacebookAuth
+//
+//  Created by Oleksandr Kurtsev on 12/09/2021.
+//
+
+#import "GodotFacebookAuth.h"
+
+@implementation GodotFacebookAuth
+
+@end
